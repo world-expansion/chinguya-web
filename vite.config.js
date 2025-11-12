3542726f6fb4821c3cf88793951656afe00eaaa4
jhoonyeel <jhoonyeel@gmail.com>
2025-11-12 22:08:52 +0900

    chore: version 환경변수 추가

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import fs from "node:fs";
+
+const pkg = JSON.parse(fs.readFileSync("./package.json", "utf-8"));
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  define: {
+    "import.meta.env.VITE_APP_VERSION": JSON.stringify(pkg.version),
+  },
 });
